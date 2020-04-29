c7640c7555c55842fc2c0583d9ffe14410cce035
zhangyang <274684940@qq.com>
2020-04-29 17:04:13 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <h1>
+      这里是scss测试
+    </h1>
+    <div class="storetest">{{storetest}}</div>
+    <el-button>Click Me</el-button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      storetest: ''
+    }
+  },
+  mounted () {
+    this.storetest = this.$store.state.count;
+    axios.get(onlinedomain+'/media/latest?category_id=10373&sort=1&size=3&page=1&channel_id=247&weight=2')
+      .then(response=>{
+        console.log(response)
+      })
+      .catch(error => {
+          console.log(error);
+      });
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1{
+  font-weight: 100;
+  color: $red;
+  background: $blue;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router';
+
+import Vuex from 'vuex'
+import store from '../store/index'  // 引入
+
+// import axios from 'axios'
+// Vue.prototype.$ajax= axios
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false
+
+Vue.use(Vuex)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  // axios,
+  components: { App },
+  template: '<App/>'
+})
